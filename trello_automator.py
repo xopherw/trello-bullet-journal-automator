c906bbcff8fc30c3bb621146a04d0180a70a275f
Christopher Wong <31772094+xopherw@users.noreply.github.com>
2021-02-03 11:33:21 +0800

    made exception for reconnection.

--- trello_automator.py
+++ trello_automator.py
@@ -1,23 +1,25 @@
 import time, datetime as dt
 from trello_func import *
-
+i = 1
 while(True):
+    try:
+        # get Bullet Journal team board and id information for quick processing
+        bullet_journal = [{'name': i.get('name'), 'board' : i.get('id'), 'list' : viewLists(i.get('id'))} for i in viewBoards() if(i.get('idOrganization') == '600062e4ff8dbe380e403bbe')]
+        
+        date = dt.datetime.now()  
+
+        if(date.year == int(bullet_journal[-1].get('name'))):
+
+            if(switchMonth(date.month) != bullet_journal[-1].get('list')[-1].get('name')): 
+                newList(date)   
+            else: 
+                if(date.day != int(viewCards(bullet_journal[-1].get('list')[-1].get('id'))[-1].get('name').strip('Day '))): 
+                    newCard(date) 
+        else:
+            newBoard(date)
+
+        time.sleep(5)
     
-    # get Bullet Journal team board and id information for quick processing
-    bullet_journal = [{'name': i.get('name'), 'board' : i.get('id'), 'list' : viewLists(i.get('id'))} for i in viewBoards() if(i.get('idOrganization') == 'YOUR_TRELLO_TEAM_ID_FROM_TRELLO_API')]
-    
-    date = dt.datetime.now()  
-
-    if(date.year == int(bullet_journal[-1].get('name'))):
-
-        if(switchMonth(date.month) != bullet_journal[-1].get('list')[-1].get('name')): 
-            newList(date)
-            newCard(date)
-        else: 
-            if(date.day != int(viewCards(bullet_journal[-1].get('list')[-1].get('id'))[-1].get('name').strip('Day '))): 
-                newCard(date)
-
-    else:
-        newBoard(date)
-
-    time.sleep(10) #NOTE: YOU CAN SET ANY TIME INTERVAL YOU WANT
+    except Exception:
+        print("Attempt to reconncet, please hold...")
+        time.sleep(5)
